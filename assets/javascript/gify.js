4a5203c94aaf518b4a13f25d6e0a9e56312e12cf
lambertco <blambertco@gmail.com>
2019-09-18 19:33:25 -0600

    psuedo code done for javascript

---
+++ assets/javascript/gify.js
@@ -0,0 +1,30 @@
+
+var keyword = '',
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=1UDXeUjPXD7dVqzhJqMItp1MDQrx5uth&limit=10";
+
+//function that displays the gif buttons
+
+//function to add new button
+
+//function to remove last button
+
+// function that displays the gifs
+//show results
+
+//put gifs in a div
+
+//pull rating of gif
+
+//pull gif
+
+//paused images
+
+//animated images
+
+//how images come in, already paused
+
+//add new div to existing divs
+
+//list of already created gifs
+
+//event listeners
