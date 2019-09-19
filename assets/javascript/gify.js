89d6372a9b10c2ea56e40f16994db8f49a31f6f8
lambertco <blambertco@gmail.com>
2019-09-18 20:30:47 -0600

    buttons displaying to page

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -1,9 +1,16 @@
 
-var keyword = '',
+var keyword = '';
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=1UDXeUjPXD7dVqzhJqMItp1MDQrx5uth&limit=10";
+var topics = ['v for vendetta', 'ice fails', 'snowboarding', 'cats', 'puppies', 'bobs burgers', 'disenchantment'];
 
 //function that displays the gif buttons
-
+function displayGifButtons() {
+    for (var i = 0; i < topics.length; i++) {
+        var gifButton = $("<button>").addClass("subjects","btn btn-primary").attr("data-name", topics[i]).text(topics[i]);
+        $("#gifButtonsView").append(gifButton);
+    }
+}
+displayGifButtons();
 //function to add new button
 
 //function to remove last button
